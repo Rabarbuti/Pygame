5fb524e6504f2c0d7be9c11133553462da874ad8
lucassatoco <lucassatocoelho@gmail.com>
2022-05-25 23:39:52 -0300

    arquivoss

--- rascunho.py
+++ rascunho.py
@@ -1,13 +1,6 @@
 import random
 import pygame
-list1 = [1, 2, 3]
-list2 = [1]
-
-intersection_set = set.intersection(set(list1), set(list2))
-intersection_list = list(intersection_set)
-
-print(intersection_list)
-#ouros
+import funcoes
 lista_ouros=['O4','O5','O6','O7','OQ','OJ','OK','OA','O2','O3']
 #espadas
 lista_espadas=['E4','E5','E6','E7','EQ','EJ','EK','EA','E2','E3']
@@ -18,75 +11,8 @@ lista_paus=['P4','P5','P6','P7','PQ','PJ','PK','PA','P2','P3']
 #lista geral de nypes
 lista_geral=[lista_ouros,lista_espadas,lista_copas,lista_paus]
 dic_valor = {'A':'ace','Q':"queen",'K':'king','J':'jack'}
-#lista_geral
-
-def tornar_carta(lista):
-    a=lista[0]
-    b=lista[1]
-    c=lista[2]
-    d=lista[3]
-    lista_geral = []
-    lista_geral.extend(a)
-    lista_geral.extend(b)
-    lista_geral.extend(c)
-    lista_geral.extend(d)
-    carta_tornada=random.choice(lista_geral)
-    return [lista_geral,carta_tornada]
-def achar_manilhas(carta_tornada,lista):
-    a=lista[0]
-    b=lista[1]
-    c=lista[2]
-    d=lista[3]
-    if carta_tornada in a:
-        indice=a.index(carta_tornada)+1
-    if carta_tornada in b:
-        indice=b.index(carta_tornada)+1
-    if carta_tornada in c:
-        indice=c.index(carta_tornada)+1
-    if carta_tornada in d:
-        indice=d.index(carta_tornada) +1
-    if indice==10:
-        indice=0
-    manilhas=[lista_ouros[indice],lista_espadas[indice],lista_copas[indice],lista_paus[indice]]
-    return manilhas
-def distribui_mao(lista):
-    lista_geral=lista[0]
-    carta_tornada=[1]
-    P1=[]
-    P2=[]
-    P3=[]
-    P4=[]
-    lista_geral.remove(carta_tornada)
-    if len(P1)<3:
-        carta_sorteada=random.choice(lista_geral)
-        P1.append(carta_sorteada)
-        lista_geral.remove(carta_sorteada)
-    if len(P2)<3:
-        carta_sorteada=random.choice(lista_geral)
-        P2.append(carta_sorteada)
-        lista_geral.remove(carta_sorteada)
-    if len(P3)<3:
-        carta_sorteada=random.choice(lista_geral)
-        P3.append(carta_sorteada)
-        lista_geral.remove(carta_sorteada)
-    if len(P4)<3:
-        carta_sorteada=random.choice(lista_geral)
-        P4.append(carta_sorteada)
-        lista_geral.remove(carta_sorteada)
-    return [P1,P2,P3,P4]
-'''def ganhador(lista_jogadores,lista_rodada,lista_geral,manilhas):
-    i=0
-    lista_manilhas_usadas=[]
-    P1=lista_jogadores[0]
-    P2=lista_jogadores[1]
-    P3=lista_jogadores[2]
-    P4=lista_jogadores[3]
-        while i<=4:
-            check=any(item   from lista_geral in manilhas)
-            if check==True:
-                if lista_rodada[i] in manilhas:
-                    lista_manilhas_usadas.append(lista_rodada[i])
-                    i+=1'''
+list1 = [1, 2, 3]
+list2 = [1]
 dicionario_imagens_ouro={}
 for carta in lista_ouros:
     if carta[1] in dic_valor:
